e501727e8c2508ebb7a4f5c3d0e83e7f8fd929d0
Heeram <bhatarkerh@gmail.com>
2026-08-20 09:40:16 +0530

    Add files via upload

---
+++ ele if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+      float a,b,c,d,e;
+      printf("please enter number");
+      scanf("%f",&a);
+      printf("\n please enter number");
+      scanf("%f",&b);
+      printf("\n please enter number");
+      scanf("%f",&c);
+      d=a+b+c;
+      e=(a+b+b)/3;
+      printf("sum:%f",d);
+      printf("\n average:%f",e);
+
+}
+
